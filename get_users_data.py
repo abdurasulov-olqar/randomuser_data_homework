3a69bbfb8ae51e98c96c2742062df51d5ba3e499
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-02-16 18:30:51 +0500

    get users

--- get_users_data.py
+++ get_users_data.py
@@ -12,3 +12,10 @@ def get_users_data(data:dict) -> list:
     Returns:
         list: users data list
     """
+    l = []
+    for user in data['results']:
+        l.append({"first_name":user['name']['first'], "last_name":user['name']['last'], "phone_number":user['phone']})
+    return l
+
+data = get_data.get_data('randomuser_data.json')
+print(get_users_data(data))
